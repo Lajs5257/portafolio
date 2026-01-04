b87f653352f6902058b643d1dc01ae2af679099c
Luis Alberto Jimenez Soto <lajs5257@gmail.com>
2026-01-03 23:38:54 -0600

    chore: update dependencies and enhance styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,20 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://lajs5257.dev',
   output: 'static',
   build: {
     format: 'directory'
   },
+  image: {
+    domains: ['avatars.githubusercontent.com']
+  },
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sitemap()]
 });
